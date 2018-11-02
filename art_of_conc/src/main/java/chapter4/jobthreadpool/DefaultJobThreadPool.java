c5b52ac91b62c7a120676548c881475e6e137400
rongbin.xie <rongbin.xie@voyageone.cn>
2018-11-02 16:32:51 +0800

    + rebuild project

--- art_of_conc/src/main/java/chapter4/jobthreadpool/DefaultJobThreadPool.java
+++ art_of_conc/src/main/java/chapter4/jobthreadpool/DefaultJobThreadPool.java
@@ -1,8 +1,6 @@
 package chapter4.jobthreadpool;
 
 
-import javafx.concurrent.Worker;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
---
+++ third-framework/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
+ * @Author jet.xie
+ * @Description:
+ * @Date: Created at 16:24 2018/11/2.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test class loader");
+    }
+}
