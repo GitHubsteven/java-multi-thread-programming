943fa87f492f6b8d021ab1d209616869b6c2e812
rongbin.xie <xrb16302@163.com>
2019-01-03 02:36:15 +0800

    + executors run case

--- art_of_conc/src/main/java/chapter10_executor/BasicExecutorTest.java
+++ art_of_conc/src/main/java/chapter10_executor/BasicExecutorTest.java
@@ -1,5 +1,7 @@
 package chapter10_executor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -13,7 +15,23 @@ public class BasicExecutorTest {
     private static ExecutorService executorService = Executors.newFixedThreadPool(3);
 
     public static void main(String[] args) {
-        executorService.execute(() -> System.out.println("runnable task"));
+        List<Runnable> runnables = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            runnables.add(() -> {
+                String threadName = Thread.currentThread().getName();
+                System.out.println(threadName + "start----------");
+                try {
+                    Thread.sleep(1000 * 3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(threadName + "end-------------");
+            });
+        }
+        for (int i = 0; i < runnables.size(); i++) {
+            executorService.execute(runnables.get(i));
+        }
+        System.out.println("main thread--------------------end!");
         executorService.shutdown();
     }
 }
